6d424a16a75534db8cff11012976ac292a535eaa
João Linhares Gomes <linharesgomes.joao@gmail.com>
2023-04-30 10:37:20 +0100

    Challenge 1: conenct mongoose to Mongo Atlas

--- myApp.js
+++ myApp.js
@@ -1,5 +1,10 @@
 require('dotenv').config();
 
+const mongoose = require('mongoose');
+
+const mongoURI = process.env.MONGO_URI;
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
